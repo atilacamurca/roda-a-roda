6f05c39df93985ea69ffb8166d531b1d766fcff9
Átila Camurça <camurca.home@gmail.com>
2022-09-05 16:51:48 -0300

    Salvando jogo atual no localStorage
    Evitando perda do progresso.
    
    Usando Vue 2 para compatibilidade de plugins.
    
    Criado botões para avançar e retroceder perguntas, assim como
    botão de reset.

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,71 @@
 <template>
-  <button type="button" v-on:click="toggleWheel" class="wheel-button">Wheel</button>
+  <div>
+    <button type="button" v-on:click="toggleWheel" class="wheel-button">Wheel</button>
 
-  <div class="wheel-popover" v-if="showWheel">
-    <button class="wheel-popover__button" type="button" @click="spinWheel('cw')">SPIN!</button>
-    <div class="wheel">
-      <div class="wheel__spinner" v-bind:style="'transform: rotate(' + wheelSpinDegs + 'deg);'">
-        <div v-for="(seg, index) in wheelSegments"
-          :key="seg + '' + index"
-          class="wheel__segment"
-          @click.native="definePts(seg)">
-          <span><template v-if="seg === 'S'">Pula</template>
-            <template v-else-if="seg === 'B'">Perde</template>
-            <template v-else>{{ seg }}pt</template></span>
+    <div class="wheel-popover" v-if="showWheel">
+      <button class="wheel-popover__button" type="button" @click="spinWheel('cw')">RODA!</button>
+      <div class="wheel">
+        <div class="wheel__spinner" v-bind:style="'transform: rotate(' + wheelSpinDegs + 'deg);'">
+          <div v-for="(seg, index) in wheelSegments" :key="seg + '' + index" class="wheel__segment"
+            @click="definePts(seg)">
+            <span><template v-if="seg === 'S'">Pula</template>
+              <template v-else-if="seg === 'B'">Perde</template>
+              <template v-else>{{ seg }}pt</template></span>
+          </div>
+          <div class="wheel__middle"><img src="logo.png"></div>
         </div>
-        <div class="wheel__middle"><img src="logo.png"></div>
       </div>
     </div>
-  </div>
 
-  <div class="board">
-    <template v-for="(word, index) in phrase" :key="word + '' + index">
-      <span class="board__letter board__letter--space" v-if="index !== 0">&nbsp;</span><span class="board__word">
-        <span v-for="(item, index2) in word"
-          :key="item + '' + index2"
-          class="board__letter"
-          v-bind:hidden="!item.visible">{{ item.char }}</span>
+    <div class="board">
+      <span v-for="(word, index) in phrase" :key="word + '' + index">
+        <span class="board__letter board__letter--space" v-if="index !== 0">&nbsp;</span>
+        <span class="board__word">
+          <span v-for="(item, index2) in word" :key="item + '' + index2" class="board__letter"
+            :hidden="!item.visible">{{ item.char }}</span>
+        </span>
       </span>
-    </template>
-  </div>
-
-  <div class="clue">{{ clue }}</div>
-  <div class="text-white text-center font-bold text-3xl">Valendo {{ currentScore }}pts</div>
-
-  <div class="controls">
-    <div class="controls__grid">
-      <button class="controls__button"
-        type="button"
-        v-for="(used, letter) in alphabetUsed"
-        :key="letter"
-        v-bind:disabled="used"
-        v-on:click="activateLetter(letter)">{{ letter }}</button>
     </div>
-    <button class="controls__button controls__button--solve" type="button" v-on:click="showBoard">Solve</button>
-  </div>
 
-  <div class="players">
-    <section class="text-gray-600 body-font my-10">
-      <div class="container px-5 py-10 mx-auto bg-white rounded">
-        <div class="flex flex-wrap -m-4 text-center justify-center">
-          <div v-for="player in players" :key="player.id" class="p-4 md:w-1/4 sm:w-1/2 w-full">
-            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg"
-              :class="[player.id === currentPlayer ? 'border-yellow-500' : '']"
-              @click.prevent="currentPlayer = player.id">
-              <h2 class="title-font font-medium text-3xl text-gray-900">{{ player.score }} pts</h2>
-              <p class="leading-relaxed">{{ player.name }}</p>
+    <div class="clue">{{ clue }}</div>
+    <div class="text-white text-center font-bold text-3xl mt-2">Valendo {{ currentScore }}pts</div>
+
+    <div class="controls">
+      <div class="controls__grid">
+        <button class="controls__button" type="button" v-for="(used, letter) in alphabetUsed" :key="letter"
+          v-bind:disabled="used" v-on:click="activateLetter(letter)">{{ letter }}</button>
+      </div>
+      <div class="flex flex-wrap">
+        <button class="controls__button controls__button--solve p-2" type="button"
+          v-on:click="showBoard">Revelar</button>
+        <button class="controls__button controls__button--solve p-2" @click.prevent="previousQuestion">&lt;</button>
+        <button class="controls__button controls__button--solve p-2" @click.prevent="nextQuestion">&gt;</button>
+        <button class="controls__button controls__button--solve p-2" @click.prevent="reset">Reset</button>
+      </div>
+    </div>
+
+    <div class="players">
+      <section class="text-gray-600 body-font my-5">
+        <div class="container px-5 py-2 mx-auto bg-white rounded">
+          <div class="flex flex-wrap -m-4 text-center justify-center">
+            <div v-for="player in players" :key="player.id" class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg"
+                :class="[player.id === currentPlayer ? 'border-yellow-500' : '']"
+                @click.prevent="currentPlayer = player.id">
+                <h2 class="title-font font-medium text-3xl text-gray-900">{{ player.score }} pts</h2>
+                <p class="leading-relaxed">{{ player.name }}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+
+
 const questions = [
   { clue: "Phrase/Quotation", phrase: "I just don't know what went wrong" },
   { clue: "On The Map", phrase: "Neighagra Falls" },
@@ -72,76 +75,99 @@ const questions = [
   { clue: "Animais", phrase: 'abelha barracuda camundongo' }
 ];
 
+const initialState = () => ({
+  players: [
+    {
+      id: 0,
+      name: 'Jogador 1',
+      score: 0
+    },
+    {
+      id: 1,
+      name: 'Jogador 2',
+      score: 0
+    },
+    {
+      id: 2,
+      name: 'Jogador 3',
+      score: 0
+    }
+  ],
+  currentPlayer: 0,
+  currentScore: 0,
+  currentPhrase: null,
+  clue: '',
+  phrase: [],
+  alphabetUsed: {
+    A: false,
+    B: false,
+    C: false,
+    D: false,
+    E: false,
+    F: false,
+    G: false,
+    H: false,
+    I: false,
+    J: false,
+    K: false,
+    L: false,
+    M: false,
+    N: false,
+    O: false,
+    P: false,
+    Q: false,
+    R: false,
+    S: false,
+    T: false,
+    U: false,
+    V: false,
+    W: false,
+    X: false,
+    Y: false,
+    Z: false
+  }
+})
+
 export default {
   data() {
     return {
-      players: [
-        {
-          id: 0,
-          name: 'Jogador 1',
-          score: 0
-        },
-        {
-          id: 1,
-          name: 'Jogador 2',
-          score: 0
-        },
-        {
-          id: 2,
-          name: 'Jogador 3',
-          score: 0
-        }
-      ],
-      currentPlayer: 0,
-      currentScore: 0,
+      ...initialState(),
       showWheel: false,
       wheelSpinDegs: 0,
       wheelSpinDuration: 0,
-      clue: '',
-      phrase: [],
-      alphabetUsed: {
-        A: false,
-        B: false,
-        C: false,
-        D: false,
-        E: false,
-        F: false,
-        G: false,
-        H: false,
-        I: false,
-        J: false,
-        K: false,
-        L: false,
-        M: false,
-        N: false,
-        O: false,
-        P: false,
-        Q: false,
-        R: false,
-        S: false,
-        T: false,
-        U: false,
-        V: false,
-        W: false,
-        X: false,
-        Y: false,
-        Z: false
-      },
       wheelSegments: ['S', 5, 1, 2, 4, 1, 3, 2, 1, 3, 1, 2, 5, 3, 1, 2, 6, 2, 4, 6, 'B', 3, 2, 1]
     }
   },
+  storage: {
+    keys: ['players', 'currentPlayer', 'currentScore', 'currentPhrase', 'clue', 'phrase', 'alphabetUsed'],
+    namespace: 'roda-a-roda'
+  },
   mounted() {
-    let item;
-    const urlParams = new URL(document.location.href).searchParams;
-    if (urlParams.get('q')) {
-      item = questions[urlParams.get('q')];
-    } else {
-      item = questions[Math.floor(Math.random() * questions.length)];
+    // let item;
+    // const urlParams = new URL(document.location.href).searchParams;
+    // if (urlParams.get('q')) {
+    //   item = questions[urlParams.get('q')];
+    // } else {
+    //   item = questions[Math.floor(Math.random() * questions.length)];
+    // }
+    // this.phrase = this.parsePhrase(item.phrase);
+    // this.clue = item.clue;
+    if (this.currentPhrase === null) {
+      this.currentPhrase = 0;
     }
-    this.phrase = this.parsePhrase(item.phrase);
-    this.clue = item.clue;
+    this.defineQuestion();
   },
   methods: {
+    defineQuestion() {
+      this.phrase = this.parsePhrase(questions[this.currentPhrase].phrase);
+      this.clue = questions[this.currentPhrase].clue;
+      // recuperar letras já reveladas
+      this.phrase.forEach((word) => {
+        word.forEach((letter) => {
+            letter.visible = this.alphabetUsed[letter.char];
+        });
+      });
+    },
     toggleWheel: function () {
       this.showWheel = !this.showWheel;
     },
@@ -216,6 +242,29 @@ export default {
       } else {
         this.currentPlayer++;
       }
+    },
+    reset() {
+      const state = initialState();
+      this.players = state.players;
+      this.currentPlayer = state.currentPlayer;
+      this.currentScore = state.currentScore;
+      this.alphabetUsed = state.alphabetUsed;
+      this.defineQuestion();
+    },
+    previousQuestion() {
+      this.currentPhrase--;
+      if (this.currentPhrase < 0) {
+        this.currentPhrase = questions.length - 1;
+      }
+      this.reset();
+    },
+    nextQuestion() {
+      this.currentPhrase++;
+      const len = questions.length;
+      if (this.currentPhrase > len - 1) {
+        this.currentPhrase = 0;
+      }
+      this.reset();
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+
+import vuejsStorage from 'vuejs-storage'
+Vue.use(vuejsStorage)
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+new Vue({
+    components: {
+        App
+    },
+    render(h) {
+        return h('App');
+    }
+}).$mount('#app')
